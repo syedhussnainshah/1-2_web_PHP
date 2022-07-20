3b5a27600f62dd180fb89acc5bf15a078617ee3e
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-07-20 14:05:47 +0500

    update

---
+++ 20-07-22/header.php
@@ -0,0 +1,8 @@
+<?php 
+$name = "Ali";
+$a = '34';
+$arry = ['HTML','CSS','BOOTSTRAP'];
+$b = 34;
+// echo gettype($int);
+?>
+<h3>Hello Header</h3>
---
+++ 20-07-22/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include_once 'header.php';
+    include_once 'header.php';
+    include_once 'header.php';
+    include_once 'header.php';
+    echo $arry[1];
+    print_r($arry);
+        // echo $name . "@gmail.com";
+    // if(1 === 1){
+    //     echo  "OK DOne";
+    // }
+    // if ($a === $b) {
+    //     echo $a + $b;
+    // } else {
+    //     echo "Your Data Types are Not same";
+    // }
+    ?>
+    <h3>Hello PHP</h3>
+</body>
+
+</html>
